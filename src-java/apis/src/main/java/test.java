a536af96a6ef07d29d31457c628f85dc0ccf7455
ruskam <mattruskamp@gmail.com>
2019-02-14 17:47:39 -0600

    test commit

--- src-java/apis/src/main/java/test.java
+++ src-java/apis/src/main/java/test.java
@@ -1,7 +1,47 @@
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class test {
     public static void main(String[] args){
+
+        port(8080);
+
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+
         get("/hello", (req, res) -> "Hello World from Spark!!!");
+
+        // matches "GET /hello/foo" and "GET /hello/bar"
+        // request.params(":name") is 'foo' or 'bar'
+        get("/hello/:name", (request, response) -> {
+            String name = request.params(":name");
+            return "Hello: " + name + "!";
+        });
+
+        get("/RPIEats", (request, response) -> {
+            //String name = request.params(":name");
+            //response.type("application/json");
+            response.body("Hello RPIEats");
+            return response;
+        });
     }
 }
