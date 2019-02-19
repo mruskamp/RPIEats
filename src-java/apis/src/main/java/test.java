36206b195940c233acfa33a5bbde289a60fc825c
Satej Sawant <satejs93@gmail.com>
2019-02-19 18:30:59 -0500

    Wrote an API to return list of restaurants

--- src-java/apis/src/main/java/test.java
+++ src-java/apis/src/main/java/test.java
@@ -1,3 +1,9 @@
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static spark.Spark.*;
 
 public class test {
@@ -49,6 +55,28 @@ public class test {
             //response.type("application/json");
             response.body("Hello RPIEats");
             return response;
+        });
+
+        get("/restaurants", (request, response) -> {
+            //String name = request.params(":name");
+            //response.type("application/json");
+            response.body("Hello RPIEats");
+            MongoClient mongoClient = MongoClients.create("mongodb://dev-team:<PASSWORD>@cluster0-shard-00-00-s62mb.mongodb.net:27017,cluster0-shard-00-01-s62mb.mongodb.net:27017,cluster0-shard-00-02-s62mb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+            MongoDatabase database = mongoClient.getDatabase("rpieats");
+            MongoCollection<Document> collection = database.getCollection("restaurants");
+            List<String> items = new ArrayList<>();
+            MongoCursor<Document> cursor = collection.find().iterator();
+
+            try{
+                while (cursor.hasNext()){
+                    items.add(cursor.next().toJson());
+                }
+            }finally {
+                cursor.close();
+            }
+
+            response.header("Content-Type","application/json");
+            return items;
         });
     }
 }
