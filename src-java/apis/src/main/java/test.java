1ed16e35eb7494d5210a09306fb12b3575d00a5d
mruskamp <mattruskamp@gmail.com>
2019-02-21 17:16:59 -0600

    Added getRestaurantStatus method to check if restaurants are open or closed at the current time.

--- src-java/apis/src/main/java/test.java
+++ src-java/apis/src/main/java/test.java
@@ -1,12 +1,32 @@
 import com.mongodb.client.*;
 import org.bson.Document;
 
+import java.sql.Time;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static spark.Spark.*;
 
 public class test {
+
+    public static String getRestaurantStatus(Document restaurant) {
+        String hours = restaurant.getString("hours");
+        String[] openAndClose = hours.split("-");
+        String open = openAndClose[0]; String close = openAndClose[1];
+        Time openTime = new Time(Integer.parseInt(open.split(":")[0]), Integer.parseInt(open.split(":")[1]), 0);
+        Time closeTime = new Time(Integer.parseInt(close.split(":")[0]), Integer.parseInt(close.split(":")[1]), 0);
+        Date timeNow = new Date();
+        Time now = new Time(timeNow.getHours(), timeNow.getMinutes(), timeNow.getSeconds());
+
+        if(now.after(openTime) && now.before(closeTime)) {
+            return "Open";
+        }
+        return "Closed";
+    }
+
     public static void main(String[] args){
 
         port(8080);
@@ -40,7 +60,6 @@ public class test {
          */
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
-
         get("/hello", (req, res) -> "Hello World from Spark!!!");
 
         // matches "GET /hello/foo" and "GET /hello/bar"
@@ -59,7 +78,7 @@ public class test {
 
         get("/restaurants", (request, response) -> {
 
-            MongoClient mongoClient = MongoClients.create("mongodb://dev-team:<PASSWORD>@cluster0-shard-00-00-s62mb.mongodb.net:27017,cluster0-shard-00-01-s62mb.mongodb.net:27017,cluster0-shard-00-02-s62mb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+            MongoClient mongoClient = MongoClients.create("mongodb://dev-team:RPIEATS@cluster0-shard-00-00-s62mb.mongodb.net:27017,cluster0-shard-00-01-s62mb.mongodb.net:27017,cluster0-shard-00-02-s62mb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
             MongoDatabase database = mongoClient.getDatabase("rpieats");
             MongoCollection<Document> collection = database.getCollection("restaurants");
             List<String> items = new ArrayList<>();
@@ -67,13 +86,16 @@ public class test {
 
             try{
                 while (cursor.hasNext()){
-                    items.add(cursor.next().toJson());
+                    Document nextRestaurant = cursor.next();
+                    nextRestaurant.put("status", getRestaurantStatus(nextRestaurant));
+                    items.add(nextRestaurant.toJson());
                 }
             }finally {
                 cursor.close();
             }
 
             response.header("Content-Type","application/json");
+
             return items;
         });
     }
